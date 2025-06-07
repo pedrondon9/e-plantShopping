efbba9c1c7ec21f46fa3398636b7347edbe547e8
pedro <stropro31@gmail.com>
2025-06-07 20:59:24 +0200

    agregando descripcion y nombre de la empresa

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,25 +5,18 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Bienvedid@ a Floréva </p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        Floréva es tu tienda online especializada en plantas de interior y exterior, que te ofrece una gran variedad para llenar de vida y frescura cualquier espacio. Con envíos rápidos y seguros, selecciona y recibe plantas saludables y listas para cuidar, ideales para decorar tu hogar, oficina o regalar a tus seres queridos.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        En Floréva sabemos que una planta es mucho más que decoración, es vida, calma y alegría. Por eso ponemos a tu alcance una selección cuidada de plantas que llegan directamente a tu puerta, para que puedas crear espacios verdes llenos de armonía, sin complicaciones y con la mejor atención personalizada
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+        Floréva es una tienda digital comprometida con la naturaleza y el bienestar, que ofrece plantas cultivadas de forma sostenible y empaquetadas cuidadosamente para llegar frescas a tu hogar. Nuestra misión es ayudarte a conectar con la naturaleza a través de plantas saludables y diseños que embellecen y purifican tus ambientes.
       </p>
     </div>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Bienvedid@ a Floréva</h1>
           <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
+          <p>Donde podras encontrar todo tipo de flores</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
+            Empezamos !
           </button>
          </div>
           <div className="aboutus_container">
